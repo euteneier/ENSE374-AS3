2a41c2bb9739b577c729d343e16c0925cb987b09
euteneier <euteneier@hotmail.com>
2016-12-06 16:27:17 -0600

    updated search

--- databasetest/database/database.java
+++ databasetest/database/database.java
@@ -5,14 +5,15 @@ import java.net.URI;
 import java.net.URL;
 import java.net.URISyntaxException;
 import java.net.MalformedURLException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class database {
 	
 	private static void openWebpage(URI uri) {
 		Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
 		if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
-			try 
-			{
+			try {
 				desktop.browse(uri);
 			} catch (Exception e) 
 			{
@@ -26,6 +27,33 @@ public class database {
 			openWebpage(url.toURI());
 		} catch (URISyntaxException e) {
 			e.printStackTrace();
+		}
+	}
+	
+	private static void testWebpage(URL url, String searchTerm) {
+		boolean good = true;
+		Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+		if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+			try {
+				BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+			} catch (Exception e) 
+			{
+				searchTerm = "https://www.drugs.com/search.php?searchterm=" + searchTerm;
+				good = false;
+			}
+			
+			try
+			{
+				if (good == false) {
+					url = new URL(searchTerm);
+				}
+				
+				openWebpage(url);
+			}
+			catch(MalformedURLException e)
+			{
+				e.printStackTrace();
+			}
 		}
 	}
 	
@@ -56,11 +84,12 @@ public class database {
 	public static void search(String searchTerm)
 	{
 		searchTerm = encode(searchTerm);
-		searchTerm = "https://www.drugs.com/search.php?searchterm=" + searchTerm;
+		String search = searchTerm;
+		searchTerm = "https://www.drugs.com/" + searchTerm + ".html";
 		try
 		{
 			URL searchURL = new URL(searchTerm);
-			openWebpage(searchURL);
+			testWebpage(searchURL, search);
 		}
 		catch(MalformedURLException e)
 		{
