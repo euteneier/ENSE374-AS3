61550f149c0cc3585695f6cd42dcb7c501d41e2c
Konstantin Kharitonov <konstantinkharitoovpro@gmail.com>
2016-12-06 21:00:56 -0600

    Photo change. (minor) added a user defined directory ask

--- photo/photo.java
+++ photo/photo.java
@@ -1,33 +1,45 @@
 package photo;
 
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
+import java.io. *;
+import java.net. *;
+import java.util.*;
 
-import javax.imageio.ImageIO;
-import javax.swing.*;
-
-public class photo extends JFrame
+public class photo 
 {
-	private BufferedImage image1;
-	private JFrame label1;
-	
-	photo()
+	public static void getImage(String imageURL, String destination) throws IOException
 	{
-		setLayout(new FlowLayout());
-		label1 = new JFrame("Image");
-		image1 = ImageIO.read(new File(drugImages));
-		add(label1);
+		URL link = new URL(imageURL);
+		InputStream input = link.openStream();
+		OutputStream output = new FileOutputStream(destination);
+		
+		byte[] by = new byte[2048];
+		int length;
+		
+		while((length = input.read(by)) != -1)
+		{
+			output.write(by, 0, length);
+		}
+		
+		input.close();
+		output.close();
 	}
 	
-	public void loadPicture()
+	public static void loadPicture() throws Exception
 	{
-		photo gui = new photo();
-		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		gui.setVisible(true);
-		gui.pack();
-		gui.setTitle("best image ever");
+		Scanner in = new Scanner(System.in);
+		System.out.println("Please Input the URL to the image you would like to upload.");
+		String input = in.nextLine();
+		System.out.println("Please name the photo");
+		String name = in.nextLine();
+		String folderLocation = null;
+		System.out.println("Please enter the destination folder.");
+		folderLocation = in.nextLine();
+		String imageLink = input;
+		String destinationFile = folderLocation + name + ".jpg";
+		
+		getImage(imageLink, destinationFile);	
+		
+		System.out.print("Image has been loaded into destination folder.");
+		in.close();
 	}
-	
-
 }
